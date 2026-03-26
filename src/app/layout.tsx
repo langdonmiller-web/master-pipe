6cd7a78aa53c432de7923cedb2d506e8f963f749
Langdon  Miller <langdon.miller@KRG-24D9-Langdon.Miller>
2026-03-26 15:30:05 -0400

    Fix CSS variables with proper hsl() wrappers in utilities layer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "./fixes.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
