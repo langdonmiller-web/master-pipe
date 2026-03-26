c2c3a25196545b0352c36a826f12864c79521bcb
Langdon  Miller <langdon.miller@KRG-24D9-Langdon.Miller>
2026-03-26 15:05:16 -0400

    Add CSS fixes for broken styling - explicit HSL color values and layout fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./fixes.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
